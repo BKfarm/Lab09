75e42f244474e9dbb6059c9dd942072a58ea814b
Ben Fahrmeier <144169391+BKfarm@users.noreply.github.com>
2023-10-31 14:35:36 -0400

    Create EnhancedForLoop.java

---
+++ EnhancedForLoop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class TicTacToe {
+
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+for(int e : data)
+        {
+            System.out.printf("%d", data[e]);
+        }
+    }
+}
